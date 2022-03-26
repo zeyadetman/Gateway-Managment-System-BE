c6aa37d536d525eb96dfd72b31219e46a9e86be0
zeyadetman <zeyadetman@gmail.com>
2022-03-26 14:38:41 +0200

    Handle API requests

--- src/api/routes/device.ts
+++ src/api/routes/device.ts
@@ -1,11 +1,43 @@
 import { Router } from "express";
 
-const route = Router();
+const deviceRoute = Router();
+const devicesRoute = Router();
 
 export default (app: Router) => {
-  app.use("/device", route);
+  app.use("/device", deviceRoute);
+  app.use("/devices", devicesRoute);
 
-  route.get("/", (req, res) => {
-    res.send("Hello World!");
+  // Create device
+  deviceRoute.post("/", (req, res) => {
+    res.send("Create device!");
+  });
+
+  // Get device by id
+  deviceRoute.get("/:id", (req, res) => {
+    const { id } = req.params;
+    res.send("Get device!" + id);
+  });
+
+  // Update device by id
+  deviceRoute.patch("/:id", (req, res) => {
+    const { id } = req.params;
+    res.send("Update device!" + id);
+  });
+
+  // Delete device by id
+  deviceRoute.delete("/:id", (req, res) => {
+    const { id } = req.params;
+    res.send("Delete device!" + id);
+  });
+
+  // Get all devices
+  devicesRoute.get("/", (req, res) => {
+    res.send("Get all devices!");
+  });
+
+  // Get all devices by gateway id
+  devicesRoute.get("/:id", (req, res) => {
+    const { id } = req.params;
+    res.send("Get devices by gatway id!" + id);
   });
 };
--- src/api/routes/gateway.ts
+++ src/api/routes/gateway.ts
@@ -1,15 +1,25 @@
 import { Router } from "express";
 
-const route = Router();
+const gatewayRoute = Router();
+const gatewaysRoute = Router();
 
 export default (app: Router) => {
-  app.use("/gateway", route);
+  app.use("/gateway", gatewayRoute);
+  app.use("/gateways", gatewaysRoute);
 
-  route.get("/", (req, res) => {
-    res.send("Get Gateway!");
+  // Create gateway
+  gatewayRoute.post("/", (req, res) => {
+    res.send("Create gateway!");
   });
 
-  route.post("/", (req, res) => {
-    res.send("Create gateway!");
+  // Get gateway by id
+  gatewayRoute.get("/:id", (req, res) => {
+    const { id } = req.params;
+    res.send("Get Gateway!" + id);
+  });
+
+  // Get All Gateways
+  gatewaysRoute.get("/", (req, res) => {
+    res.send("Get all gateways!");
   });
 };
