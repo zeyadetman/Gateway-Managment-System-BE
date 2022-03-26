ea64dce8979f2752fdd879b3d6186f023be258b7
zeyadetman <zeyadetman@gmail.com>
2022-03-26 20:16:20 +0200

    Handle Gateways

--- src/api/routes/gateway.ts
+++ src/api/routes/gateway.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import Gateway from "../../services/gateway";
+import { PER_PAGE } from "../../utils/helpers";
 
 const gatewayRoute = Router();
 const gatewaysRoute = Router();
@@ -20,14 +21,32 @@ export default (app: Router) => {
     }
   });
 
-  // Get gateway by id
-  gatewayRoute.get("/:id", (req, res) => {
-    const { id } = req.params;
-    res.send("Get Gateway!" + id);
+  // Get gateway by serial number
+  gatewayRoute.get("/:serial_number", async (req, res, next) => {
+    try {
+      const { serial_number } = req.params;
+      const response = await gatewayInstance.getGatewayBySerialNumber(
+        serial_number
+      );
+      if (response) {
+        res.status(200).json(response);
+      } else {
+        return next({ status: 404, message: "Gateway not found" });
+      }
+    } catch (error: any) {
+      return next(error);
+    }
   });
 
   // Get All Gateways
-  gatewaysRoute.get("/", (req, res) => {
-    res.send("Get all gateways!");
+  gatewaysRoute.get("/", async (req, res, next) => {
+    try {
+      const page: number = Number(req.query.page) || 1;
+      const limit: number = Number(req.query.limit) || PER_PAGE;
+      const response = await gatewayInstance.getAllGateways(page, limit);
+      res.status(200).json(response);
+    } catch (error: any) {
+      return next(error);
+    }
   });
 };
--- src/services/gateway.ts
+++ src/services/gateway.ts
@@ -3,7 +3,7 @@ import GatewayModel from "../models/Gateway";
 import { gatewayValidationSchema } from "../validations/gateway";
 
 export default class Gateway {
-  public async createGateway(body: IGatewayInputDTO) {
+  async createGateway(body: IGatewayInputDTO) {
     try {
       const { error, value } = gatewayValidationSchema.validate(body);
       if (error?.message) {
@@ -16,10 +16,36 @@ export default class Gateway {
       throw new Error(error);
     }
   }
-  getGatewayById() {
-    return;
+
+  async getGatewayBySerialNumber(serialnumber: string) {
+    try {
+      const { error, value } = gatewayValidationSchema
+        .extract("serialnumber")
+        .validate(serialnumber);
+      if (error?.message) {
+        throw new Error(error.message);
+      }
+
+      const res = await GatewayModel.findOne({ serialnumber: value });
+      return res?.toJSON();
+    } catch (error: any) {
+      throw new Error(error);
+    }
   }
-  getAllGateways() {
-    return;
+
+  async getAllGateways(page: number, limit: number) {
+    try {
+      const count = await GatewayModel.countDocuments();
+      const res = await GatewayModel.find()
+        .sort({
+          name: "asc",
+        })
+        .skip((page - 1) * limit)
+        .limit(limit);
+      const data = res.map((gateway) => gateway.toJSON());
+      return { data, count, page: Number(page), limit: Number(limit) };
+    } catch (error: any) {
+      throw new Error(error);
+    }
   }
 }
